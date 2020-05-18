1b732c0c6095c7405831d4740851b9d51e64a65a
Pablito Parungao Romero Jr / Developer <ppromerojr@gmail.com>
2020-05-18 22:49:44 +0800

    style: moved sign out to settings

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,21 +1,34 @@
 import React, { useContext, memo } from "react"
-import { View } from "react-native"
+import { View, TouchableOpacity } from "react-native"
 import styled from "@emotion/native"
+import * as Updates from "expo-updates"
 
 import withScreen from "../../utils/hoc/createScreen"
 import { AppContext } from "../Main"
 import Button from "../components/Button"
+import { normalize } from "../../utils/scale"
 
 const Text = styled.Text(({ theme }) => ({
     color: theme.main.color,
 }))
 
 const Settings = memo(() => {
-    const { activeTheme, toggleTheme } = useContext(AppContext)
+    const { appState, actions, activeTheme, toggleTheme } = useContext(AppContext)
 
     return (
-        <View>
+        <View style={{ marginTop: normalize(20) }}>
             <Button title={activeTheme === "light" ? "Dark Mode" : "Light Mode"} onPress={toggleTheme} />
+            <View style={{ marginTop: normalize(50), justifyContent: "center", alignItems: "center" }}>
+                {appState.userToken && (
+                    <TouchableOpacity
+                        onPress={() => {
+                            actions.signOut()
+                        }}>
+                        <Text>Sign out</Text>
+                    </TouchableOpacity>
+                )}
+                <Text>{Updates.manifest.version}</Text>
+            </View>
         </View>
     )
 })
