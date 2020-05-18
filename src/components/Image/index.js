96a69dc50d4ff00d77528bbe534b8f334d0c9151
Pablito Parungao Romero Jr / Developer <ppromerojr@gmail.com>
2020-05-18 22:50:24 +0800

    style: clean image component

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react"
+import React, { useState } from "react"
 import { Image, View, Text } from "react-native"
 
 const LazyImage = ({ source, ...rest }) => {
