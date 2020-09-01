dbe56dd48fd45f4fa3f37abde46e2f1803207c65
Pabs Romero Jr <ppromerojr@gmail.com>
2020-09-01 20:17:27 +0800

    chore: link code-push analytics

--- android/app/src/main/java/com/zinevy/pos/MainApplication.java
+++ android/app/src/main/java/com/zinevy/pos/MainApplication.java
@@ -15,6 +15,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -25,6 +26,11 @@ public class MainApplication extends Application implements ReactApplication {
           return BuildConfig.DEBUG;
         }
 
+        @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
+        }
+        
         @Override
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
